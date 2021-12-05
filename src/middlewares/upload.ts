14a083db5ea7ce76b13d6c5ecd7de5181b177328
Alejandro Cortell Marín <alejandrocortellmarin@gmail.com>
2021-12-05 18:01:58 +0100

    Save files in sub folders

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,9 +1,16 @@
 const util = require('util')
+const fs = require('fs')
 const multer = require('multer')
+import controllerOrganization from '../controllers/organizations.controller'
+import controllerType from '../controllers/documentsTypes.controller'
 
 let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, `./uploads/`)
+    destination: async (req, file, cb) => {
+        const organization = await controllerOrganization.getOrganization(req.body.organization)
+        const type = await controllerType.getDocumentType(+req.body.documentType)
+        const path = `./uploads/${organization.name}/${type.name}`
+        fs.mkdirSync(path, { recursive: true })
+        cb(null, path)
     },
 })
 

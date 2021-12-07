82178be85d81726c7db4e621095e07f9317748a3
Alejandro Cortell Marín <alejandrocortellmarin@gmail.com>
2021-12-07 20:37:19 +0100

    Allow modify users different roles

--- src/middlewares/sameUser.ts
+++ src/middlewares/sameUser.ts
@@ -1,6 +1,13 @@
-const sameUser = (req, res, next) => {
+import controllerUser from '../controllers/users.controller'
+
+const sameUser = async (req, res, next) => {
+    const user = await controllerUser.getUser(req.decoded.id)
     const id = parseInt(req.params.id)
-    if (id === req.decoded.id) {
+    const allowRoles = [1, 2, 3]
+
+    if (allowRoles.includes(user.RoleId)) {
+        next()
+    } else if (id === req.decoded.id) {
         next()
     } else {
         res.status(203).send({ message: 'Unauthorized' })

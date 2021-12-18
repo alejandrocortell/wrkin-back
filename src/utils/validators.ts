5f9c20b9212943e758b807492279dece6dec91f2
Alejandro Cortell Marín <alejandrocortellmarin@gmail.com>
2021-12-18 11:20:19 +0100

    Validators

---
+++ src/utils/validators.ts
@@ -0,0 +1,16 @@
+export const trim = (text) => {
+    return text.trim()
+}
+
+export const isString = (text) => {
+    return typeof text === 'string' ? true : false
+}
+
+export const isNumber = (number) => {
+    return typeof number === 'number' ? true : false
+}
+
+export const isDate = (date) => {
+    const a = new Date(date)
+    return a.getTime() === a.getTime()
+}

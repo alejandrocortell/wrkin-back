f327c4c8078a59cbd096de67cbad69985a8683be
Alejandro Cortell Marín <alejandrocortellmarin@gmail.com>
2022-01-09 17:00:04 +0100

    Add cors

--- app.ts
+++ app.ts
@@ -1,12 +1,29 @@
 require('dotenv').config()
 const bodyparser = require('body-parser')
 const express = require('express')
+const cors = require('cors')
 const config = require('./src/config/config')
 
 import router from './src/routes'
 
 const server = express()
 
+const allowedOrigins = ['http://localhost:3000']
+
+server.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true)
+            if (allowedOrigins.indexOf(origin) === -1) {
+                const msg =
+                    'The CORS policy for this site does not ' +
+                    'allow access from the specified Origin.'
+                return callback(new Error(msg), false)
+            }
+            return callback(null, true)
+        },
+    })
+)
 server.set('key', config.keyJWT)
 server.use(bodyparser.urlencoded({ extended: true }))
 server.use(bodyparser.json())
